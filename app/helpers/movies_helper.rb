808dc9f9bc626ca1da680b5126e174a1926e3caf
Steven Nunez <snunez@cyrusinnovation.com>
2013-07-10 21:25:23 -0400

    Add boostrap. First pass at styling

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+  def errors_for(errors)
+    if errors.any?
+      "<p class='text-error'>#{errors.map(&:capitalize).join("<br />")}</p>".html_safe
+    end
+  end
 end
--- test/helpers/movies_helper_test.rb
+++ test/helpers/movies_helper_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class MoviesHelperTest < ActionView::TestCase
+  test "returns properly formatted errors from array" do
+    actual_result = errors_for(["can't be blank", "Your description is far too short"])
+    expected_result = "<p class='text-error'>Can't be blank<br />Your description is far too short</p>"
+
+    assert_equal expected_result, actual_result
+  end
+
+  test "returns false if array is empty" do
+    assert_nil errors_for([])
+  end
 end
